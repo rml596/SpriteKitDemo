9bdd1c02c5b08e45fef7a61d72366ed9a1e1d25d
rml596 <rml596@gmail.com>
2015-04-20 21:03:36 -0400

    Initial Commit

---
+++ SpriteKitDemo/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  SpriteKitDemo
+//
+
+//  Copyright (c) 2015 Robert Lynch Development. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ SpriteKitDemo/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  SpriteKitDemo
+//
+
+//  Copyright (c) 2015 Robert Lynch Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
